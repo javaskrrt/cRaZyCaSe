6d49599dd0c0fa6c96c52906e8da82765a7420f3
JavaSkrrt <javaskrrt@gmail.com>
2024-12-18 23:21:39 -0600

    configure build files, types defs, update package.json

---
+++ dist/src/index.d.ts
@@ -0,0 +1,14 @@
+/**
+ * mOcKcAsE - Transform your boring strings into mOcKcAsE strings!
+ *
+ * @param {string} input - The string to transform into mOcKcAsE.
+ * @param {object} [options] - Configuration options.
+ * @param {boolean} [options.capitalizeFirst=false] - Whether the first character should be uppercase.
+ *
+ * @returns {string} The mOcKcAsE version of the input string.
+ */
+export interface MockCaseOptions {
+    capitalizeFirst?: boolean;
+}
+export declare function mOcKcAsE(input: string, options?: MockCaseOptions): string;
+//# sourceMappingURL=index.d.ts.map
